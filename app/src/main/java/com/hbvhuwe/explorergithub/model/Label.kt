fd25269212e78e45f9cae9a8e0cdc80e27cb514a
hbvhuwe <chernonog.vova@gmail.com>
2018-10-28 16:36:40 +0200

    Implement issues tab in repository activity

---
+++ app/src/main/java/com/hbvhuwe/explorergithub/model/Label.kt
@@ -0,0 +1,9 @@
+package com.hbvhuwe.explorergithub.model
+
+data class Label(
+        val id: Int,
+        val url: String,
+        val name: String,
+        val color: String,
+        val default: Boolean
+)
